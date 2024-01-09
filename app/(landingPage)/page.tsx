ecbdbc47ed8b4a7b72b6be2838045318c5b3ad63
Kashif Nawaz <danishn786@gmail.com>
2024-01-09 15:06:08 +0530

    Added UI Components

---
+++ app/(landingPage)/page.tsx
@@ -0,0 +1,9 @@
+const MainLandingPage = () => {
+    return (
+        <div className= "flex justify-center text-4xl text-gray-500 underline" >
+           Welcome To the Task Manegment App
+        </div>
+    )
+}
+
+export default MainLandingPage;
